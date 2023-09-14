e230fabe10e6c956d4bceab15a6f8a4c5b7ba276
asad523892 <asadmahesar21@gmail.com>
2023-09-15 00:57:32 +0500

    pushing done

--- eproject/eproject/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace eproject.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
